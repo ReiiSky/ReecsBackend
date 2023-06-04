834cdb7461da96cf7094409e6d0b6f62dbcdf129
ReiiSky <danielarya185@gmail.com>
2023-06-04 13:02:52 +0700

    add: change config pg

--- controllers/Database.py
+++ controllers/Database.py
@@ -1,11 +1,10 @@
 import psycopg2
 
 conn = psycopg2.connect(
-        database="reecs",
-        user = "postgres",
-        password = "reecs-password",
-        host = "127.0.0.1",
-        port = "5432",
-    )
-
+    database="reecs",
+    user = "postgres",
+    password = "reecs-password",
+    host = "127.0.0.1",
+    port = "5432",
+)
 conn.autocommit = True
